a8d13c16ab58b359e3d6f8dc6a5a793f25304897
Diraj <dirajravikumar@my.jcu.edu.au>
2016-11-19 19:38:59 +0800

    Included MinutesConversion and NauticalMiles

---
+++ src/NauticalMiles.java
@@ -0,0 +1,17 @@
+public class NauticalMiles {
+
+    public static void main(String[] args)
+    {
+        double NM = 1, KM, M;
+
+        KM = NM * 1.852;
+        M = NM * 1.150779;
+
+
+        System.out.println("The amount of Kilometres is " + KM + " and the amount of Miles is " + M);
+
+
+
+
+    }
+}
